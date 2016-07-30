b422b602bdacaab915de9cd389b5c579fa808674
yycho0108 <jchocholate@gmail.com>
2016-07-30 18:56:09 -0400

    saving changes

--- server.js
+++ server.js
@@ -126,6 +126,6 @@ app.post('/delete',function(req,res){
 
 });
 
-app.listen(process.env.PORT || 8000, function () {
+app.listen(process.env.PORT || 8001, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
